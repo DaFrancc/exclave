3a855bac2f7f5036bcb78b9730cf9a92d298941d
Vi_P <xenonaught@gmail.com>
2021-02-17 21:10:09 -0600

    fixed transition for nav bar

--- logic.js
+++ logic.js
@@ -2,15 +2,29 @@ var isUp = false;
 function toggleTab(t) {
     let nav = document.getElementById("nav-tab");
     let tb = document.getElementById("top-bar");
-    if(!isUp){
-        nav.style.transform = "translateY(-250%)";
-        tb.style.transform = "translateY(-100%)";
-        document.getElementById("toggleTab").innerHTML = "Open";
-        isUp = true;
-    } else {
-        nav.style.transform = "translateY(-100%)";
-        tb.style.transform = "translateY(0%)";
-        document.getElementById("toggleTab").innerHTML = "Close";
-        isUp = false;
+    if (window.matchMedia("(orientation: portrait)").matches) {
+        if(!isUp){
+            nav.style.transform = "translateY(-400%)";
+            tb.style.transform = "translateY(-100%)";
+            document.getElementById("toggleTab").innerHTML = "Open";
+            isUp = true;
+        } else {
+            nav.style.transform = "translateY(-200%)";
+            tb.style.transform = "translateY(0%)";
+            document.getElementById("toggleTab").innerHTML = "Close";
+            isUp = false;
+        }
+    } else if (window.matchMedia("(orientation: landscape)").matches) {
+        if(!isUp){
+            nav.style.transform = "translateY(-250%)";
+            tb.style.transform = "translateY(-100%)";
+            document.getElementById("toggleTab").innerHTML = "Open";
+            isUp = true;
+        } else {    
+            nav.style.transform = "translateY(-100%)";
+            tb.style.transform = "translateY(0%)";
+            document.getElementById("toggleTab").innerHTML = "Close";
+            isUp = false;
+        }
     }
 }

74b87b2fa4ca852fd2dbbf7b26a94579df051b45
DaFrancc <xenonaught@gmail.com>
2021-04-05 16:18:53 -0500

    changes

--- logic.js
+++ logic.js
@@ -4,19 +4,19 @@ function toggleTab() {
     let tb = document.getElementById("top-bar");
     if (window.matchMedia("(orientation: portrait)").matches) {
         if(!isUp){
-            nav.style.transform = "translateY(-400%)";
+            nav.style.transform = "translateY(-500%)";
             tb.style.transform = "translateY(-100%)";
             document.getElementById("toggleTab").innerHTML = "Open";
             isUp = true;
         } else {
-            nav.style.transform = "translateY(-200%)";
+            nav.style.transform = "translateY(-250%)";
             tb.style.transform = "translateY(0%)";
             document.getElementById("toggleTab").innerHTML = "Close";
             isUp = false;
         }
     } else if (window.matchMedia("(orientation: landscape)").matches) {
         if(!isUp){
-            nav.style.transform = "translateY(-250%)";
+            nav.style.transform = "translateY(-300%)";
             tb.style.transform = "translateY(-100%)";
             document.getElementById("toggleTab").innerHTML = "Open";
             isUp = true;

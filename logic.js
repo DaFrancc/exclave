3e11f41a3ee770c12af28846b0aaf753cf6fbc77
Francisco <65142449+DaFrancc@users.noreply.github.com>
2021-04-13 11:32:32 -0500

    grammar

--- logic.js
+++ logic.js
@@ -6,24 +6,24 @@ function toggleTab() {
         if(!isUp){
             nav.style.transform = "translateY(-500%)";
             tb.style.transform = "translateY(-100%)";
-            document.getElementById("toggleTab").innerHTML = "Open";
+            document.getElementById("toggleTab").innerHTML = "Show";
             isUp = true;
         } else {
             nav.style.transform = "translateY(-250%)";
             tb.style.transform = "translateY(0%)";
-            document.getElementById("toggleTab").innerHTML = "Close";
+            document.getElementById("toggleTab").innerHTML = "Hide";
             isUp = false;
         }
     } else if (window.matchMedia("(orientation: landscape)").matches) {
         if(!isUp){
             nav.style.transform = "translateY(-300%)";
             tb.style.transform = "translateY(-100%)";
-            document.getElementById("toggleTab").innerHTML = "Open";
+            document.getElementById("toggleTab").innerHTML = "Show";
             isUp = true;
         } else {    
             nav.style.transform = "translateY(-100%)";
             tb.style.transform = "translateY(0%)";
-            document.getElementById("toggleTab").innerHTML = "Close";
+            document.getElementById("toggleTab").innerHTML = "Hide";
             isUp = false;
         }
     }
